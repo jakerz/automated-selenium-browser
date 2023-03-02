3e8927bc9164bf76e61ccbe6279dd37805cabe45
Jake Ramirez <jake2ramirez@gmail.com>
2023-03-01 20:27:05 -0800

    added nadex site example

--- simplebrowser.py
+++ simplebrowser.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
+
+webpage_url = "http://www.python.org"
+webpage_url = "https://www.nadex.com"
 
 # driver = webdriver.Firefox()
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+driver.get(webpage_url)
+# assert "Python" in driver.title
+# elem = driver.find_element(By.NAME, "q")
+time.sleep(1)
+# elem.clear()
+# elem.send_keys(Keys.RETURN)
+time.sleep(3)
 assert "No results found." not in driver.page_source
 driver.close()
 
